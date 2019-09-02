193068a09ce92b0f11ce71430dcf58b5f6aa0b4e
KevinZuniga98 <52494862+KevinZuniga98@users.noreply.github.com>
2019-09-02 12:04:53 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*void cambiarPorValor(int unDato);
+void cambiarPorReferencia(int* pDato);*/
+int dameUnEntero(char*mensaje, int max, int cantidad, int *pNumero );
+
+int main()
+{
+
+     int unNumero;
+     int sePudo;
+
+     sePudo = dameUnEntero("\nIngresa tu edad\n",100,3, &unNumero);
+
+     if(sePudo == 0)
+        {
+        printf("\nno se pudo cargar\n");
+
+        }
+        else
+            {
+            printf("\nel numero es :%d\n", unNumero);
+            }
+
+
+     return 0;
+}
+
+
+int dameUnEntero(char* mensaje,int max, int cantidad, int *pNumero)
+{
+    int contador = 0;
+    printf("%s", mensaje);
+    scanf("%d", pNumero);
+    while (*pNumero > max)
+        {
+            contador ++;
+            printf("%s", mensaje);
+            scanf("%d", pNumero);
+
+            if (contador = cantidad)
+                {
+                    return 0;
+                }
+        }
+    return 1;
+}
